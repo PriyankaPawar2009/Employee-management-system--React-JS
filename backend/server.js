3d9dc2345fbe3d5dd475bf4275a69108e402ea10
PriyankaPawar2009 <priyanka@rasheedfoundation.org>
2025-07-09 17:49:55 +0530

    server.js backend

--- backend/server.js
+++ backend/server.js
@@ -467,6 +467,6 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// Start server
+// Starting the  server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
